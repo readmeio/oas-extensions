a0ea00f6d779871e7b412c906fcc84d30237fea7
Jon Ursenbach <erunion@users.noreply.github.com>
2023-04-03 09:51:41 -0700

    chore: moving tests over to jest (#149)

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,43 +1,39 @@
-/* eslint-disable mocha/no-setup-in-describe */
 import petstore from '@readme/oas-examples/3.0/json/petstore.json';
-import { expect } from 'chai';
 import Oas from 'oas';
 
 import * as extensions from '../src';
 
-describe('oas-extensions', function () {
-  [
-    'CODE_SAMPLES',
-    'EXPLORER_ENABLED',
-    'HEADERS',
-    'METRICS_ENABLED',
-    'PROXY_ENABLED',
-    'SAMPLES_ENABLED',
-    'SAMPLES_LANGUAGES',
-    'SEND_DEFAULTS',
-    'SIMPLE_MODE',
-  ].forEach(extension => {
-    it(`\`${extension}\` extension should have a default value`, function () {
-      expect(extensions.defaults).to.have.property(extensions[extension]);
-    });
+describe('oas-extensions', () => {
+  it.each([
+    ['CODE_SAMPLES'],
+    ['EXPLORER_ENABLED'],
+    ['HEADERS'],
+    ['METRICS_ENABLED'],
+    ['PROXY_ENABLED'],
+    ['SAMPLES_ENABLED'],
+    ['SAMPLES_LANGUAGES'],
+    ['SEND_DEFAULTS'],
+    ['SIMPLE_MODE'],
+  ])('%s should have a default value', extension => {
+    expect(extensions.defaults).toHaveProperty(extensions[extension]);
   });
 
-  describe('#getExtension', function () {
-    it("should not throw an exception if `Oas` doesn't have an API definition", function () {
+  describe('#getExtension', () => {
+    it("should not throw an exception if `Oas` doesn't have an API definition", () => {
       const oas = Oas.init(undefined);
-      expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas)).to.have.length(7);
+      expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas)).toHaveLength(7);
     });
 
-    it("should not throw an exception if `Operation` doesn't have an API definition", function () {
+    it("should not throw an exception if `Operation` doesn't have an API definition", () => {
       const oas = Oas.init(undefined);
       const operation = oas.operation('/pet', 'post');
-      expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas, operation)).to.have.length(7);
+      expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas, operation)).toHaveLength(7);
     });
 
-    describe('oas-level extensions', function () {
-      it('should use the default extension value if the extension is not present', function () {
+    describe('oas-level extensions', () => {
+      it('should use the default extension value if the extension is not present', () => {
         const oas = Oas.init(petstore);
-        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas)).to.deep.equal([
+        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas)).toStrictEqual([
           'shell',
           'node',
           'ruby',
@@ -48,7 +44,7 @@ describe('oas-extensions', function () {
         ]);
       });
 
-      it('should locate an extensions under `x-readme`', function () {
+      it('should locate an extensions under `x-readme`', () => {
         const oas = Oas.init({
           ...petstore,
           'x-readme': {
@@ -56,24 +52,24 @@ describe('oas-extensions', function () {
           },
         });
 
-        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas)).to.deep.equal(['swift']);
+        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas)).toStrictEqual(['swift']);
       });
 
-      it('should locate an extensions listed at the root', function () {
+      it('should locate an extensions listed at the root', () => {
         const oas = Oas.init({ ...petstore, [`x-${extensions.EXPLORER_ENABLED}`]: false });
-        expect(extensions.getExtension(extensions.EXPLORER_ENABLED, oas)).to.be.false;
+        expect(extensions.getExtension(extensions.EXPLORER_ENABLED, oas)).toBe(false);
       });
 
-      it('should not throw if `x-readme` is not an object', function () {
+      it('should not throw if `x-readme` is not an object', () => {
         const oas = Oas.init({
           ...petstore,
           'x-readme': true,
         });
 
-        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas)).to.have.length(7);
+        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas)).toHaveLength(7);
       });
 
-      it('should not pick up the `code-samples` extension', function () {
+      it('should not pick up the `code-samples` extension', () => {
         const oas = Oas.init({
           ...petstore,
           'x-readme': {
@@ -87,21 +83,21 @@ describe('oas-extensions', function () {
           },
         });
 
-        expect(extensions.getExtension(extensions.CODE_SAMPLES, oas)).to.be.undefined;
+        expect(extensions.getExtension(extensions.CODE_SAMPLES, oas)).toBeUndefined();
       });
     });
 
-    describe('operation-level', function () {
+    describe('operation-level', () => {
       let oas;
 
-      beforeEach(function () {
+      beforeEach(() => {
         oas = Oas.init(petstore);
       });
 
-      it('should use the default extension value if the extension is not present', function () {
+      it('should use the default extension value if the extension is not present', () => {
         const operation = oas.operation('/pet', 'post');
 
-        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas, operation)).to.deep.equal([
+        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas, operation)).toStrictEqual([
           'shell',
           'node',
           'ruby',
@@ -112,47 +108,47 @@ describe('oas-extensions', function () {
         ]);
       });
 
-      it('should locate an extensions under `x-readme`', function () {
+      it('should locate an extensions under `x-readme`', () => {
         const operation = oas.operation('/pet', 'post');
         operation.schema['x-readme'] = {
           [extensions.SAMPLES_LANGUAGES]: ['swift'],
         };
 
-        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas, operation)).to.deep.equal(['swift']);
+        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas, operation)).toStrictEqual(['swift']);
       });
 
-      it('should locate an extensions listed at the root', function () {
+      it('should locate an extensions listed at the root', () => {
         const operation = oas.operation('/pet', 'post');
         operation.schema[`x-${extensions.EXPLORER_ENABLED}`] = false;
 
-        expect(extensions.getExtension(extensions.EXPLORER_ENABLED, oas, operation)).to.be.false;
+        expect(extensions.getExtension(extensions.EXPLORER_ENABLED, oas, operation)).toBe(false);
       });
 
-      it('should not throw if `x-readme` is not an object', function () {
+      it('should not throw if `x-readme` is not an object', () => {
         const operation = oas.operation('/pet', 'post');
         operation.schema['x-readme'] = true;
 
-        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas)).to.have.length(7);
+        expect(extensions.getExtension(extensions.SAMPLES_LANGUAGES, oas)).toHaveLength(7);
       });
     });
   });
 
-  describe('#isExtensionValid()', function () {
-    it('should validate that `x-readme` is an object', function () {
+  describe('#isExtensionValid()', () => {
+    it('should validate that `x-readme` is an object', () => {
       expect(() => {
         extensions.validateExtension(extensions.EXPLORER_ENABLED, Oas.init({ 'x-readme': [] }));
-      }).to.throw(/must be of type "Object"/);
+      }).toThrow(/must be of type "Object"/);
 
       expect(() => {
         extensions.validateExtension(extensions.EXPLORER_ENABLED, Oas.init({ 'x-readme': false }));
-      }).to.throw(/must be of type "Object"/);
+      }).toThrow(/must be of type "Object"/);
 
       expect(() => {
         extensions.validateExtension(extensions.EXPLORER_ENABLED, Oas.init({ 'x-readme': null }));
-      }).to.throw(/must be of type "Object"/);
+      }).toThrow(/must be of type "Object"/);
     });
 
-    [
+    describe.each([
       [
         'CODE_SAMPLES',
         [
@@ -174,55 +170,52 @@ describe('oas-extensions', function () {
       ['SAMPLES_LANGUAGES', ['swift'], {}, 'Array'],
       ['SEND_DEFAULTS', true, 'absolutely not', 'Boolean'],
       ['SIMPLE_MODE', true, 'absolutely not', 'Boolean'],
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ].forEach(([extension, validValue, invalidValue, expectedType]: [string, any, any, string]) => {
-      describe(`${extension}`, function () {
-        describe('should allow valid extensions', function () {
-          it('should allow at the root level', function () {
-            expect(() => {
-              extensions.validateExtension(
-                extensions[extension],
-                Oas.init({ [`x-${extensions[extension]}`]: validValue })
-              );
-            }).not.to.throw();
-          });
-
-          it('should allow if nested in `x-readme`', function () {
-            expect(() => {
-              extensions.validateExtension(
-                extensions[extension],
-                Oas.init({
-                  'x-readme': {
-                    [extensions[extension]]: validValue,
-                  },
-                })
-              );
-            }).not.to.throw();
-          });
+    ])('%s', (extension, validValue, invalidValue, expectedType) => {
+      describe('should allow valid extensions', function () {
+        it('should allow at the root level', function () {
+          expect(() => {
+            extensions.validateExtension(
+              extensions[extension],
+              Oas.init({ [`x-${extensions[extension]}`]: validValue })
+            );
+          }).not.toThrow();
         });
 
-        describe('should fail on invalid extension values', function () {
-          it('should error if at the root level', function () {
-            expect(() => {
-              extensions.validateExtension(
-                extensions[extension],
-                Oas.init({ [`x-${extensions[extension]}`]: invalidValue })
-              );
-            }).to.throw(new RegExp(`"x-${extensions[extension]}" must be of type "${expectedType}"`));
-          });
+        it('should allow if nested in `x-readme`', function () {
+          expect(() => {
+            extensions.validateExtension(
+              extensions[extension],
+              Oas.init({
+                'x-readme': {
+                  [extensions[extension]]: validValue,
+                },
+              })
+            );
+          }).not.toThrow();
+        });
+      });
 
-          it('should error if nested in `x-readme`', function () {
-            expect(() => {
-              extensions.validateExtension(
-                extensions[extension],
-                Oas.init({
-                  'x-readme': {
-                    [extensions[extension]]: invalidValue,
-                  },
-                })
-              );
-            }).to.throw(new RegExp(`"x-readme.${extensions[extension]}" must be of type "${expectedType}"`));
-          });
+      describe('should fail on invalid extension values', function () {
+        it('should error if at the root level', function () {
+          expect(() => {
+            extensions.validateExtension(
+              extensions[extension],
+              Oas.init({ [`x-${extensions[extension]}`]: invalidValue })
+            );
+          }).toThrow(new RegExp(`"x-${extensions[extension]}" must be of type "${expectedType}"`));
+        });
+
+        it('should error if nested in `x-readme`', function () {
+          expect(() => {
+            extensions.validateExtension(
+              extensions[extension],
+              Oas.init({
+                'x-readme': {
+                  [extensions[extension]]: invalidValue,
+                },
+              })
+            );
+          }).toThrow(new RegExp(`"x-readme.${extensions[extension]}" must be of type "${expectedType}"`));
         });
       });
     });
